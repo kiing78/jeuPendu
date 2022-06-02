c7dd79572ba6a88d249f534855d6671cdf1df7eb
EmmanuelKONTZLER <kontzleremmanuel@yahoo.fr>
2022-06-02 15:21:27 +0200

    fct : Lancer le jeu - renseigner un mot mystere

---
+++ PenduGIT.py
@@ -0,0 +1,35 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      emman
+#
+# Created:     02/06/2022
+# Copyright:   (c) emman 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+
+liste = [
+    [1, 1, ['FOOTBALL', 'TENNIS', 'BASKET']],
+    [1, 2, ['PETANQUE', 'AEROBIQUE', 'LUGE']],
+    [2, 1, ['PARIS', 'LYON', 'MARSEILLE']],
+    [2, 2, ['MELUN', 'EVRY', 'TARBES']],
+    [3, 1, ['MICKEY', 'DONALD', 'PLUTO']],
+    [3, 2, ['PICSOU', 'DINGO', 'MINNIE']],
+]
+
+def pendu():
+    '''Lance le pendu'''
+    lettresProposées = []
+    word=input("Renseignez le mot à deviner").upper()
+    secret = []
+
+
+
+
+
+
+
+
+pendu()
